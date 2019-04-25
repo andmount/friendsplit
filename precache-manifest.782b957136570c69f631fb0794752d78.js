8a1970d7ab4ada9cdb9fe7f65e21c31a10b54835
trickster <andmount@gmail.com>
2019-04-25 16:58:17 +0300

    Updates

---
+++ precache-manifest.782b957136570c69f631fb0794752d78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f91b800e16d44d649af5bde1fff9ac8d",
+    "url": "/friendsplit/static/media/logo.f91b800e.png"
+  },
+  {
+    "revision": "fd56dd65185805755f710de97eec4edf",
+    "url": "/friendsplit/static/media/joe.fd56dd65.png"
+  },
+  {
+    "revision": "94056a9a6ed312d66c04",
+    "url": "/friendsplit/static/js/runtime~main.6549dedd.js"
+  },
+  {
+    "revision": "7c842717459be49cf31a",
+    "url": "/friendsplit/static/js/main.6b7e233f.chunk.js"
+  },
+  {
+    "revision": "292159d9ee7f8e0cde4b",
+    "url": "/friendsplit/static/js/2.5661c722.chunk.js"
+  },
+  {
+    "revision": "7c842717459be49cf31a",
+    "url": "/friendsplit/static/css/main.c35d5c1a.chunk.css"
+  },
+  {
+    "revision": "76784da978b33c73fd8a64eb64c0f8ee",
+    "url": "/friendsplit/index.html"
+  }
+];
